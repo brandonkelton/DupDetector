e14962362b9088bb41eb13f64666d636c7ba99a9
Andrew Lee Pino <andrew.pino@outlook.com>
2018-10-01 20:51:38 -0600

    removed grid rows and cols in main window and added close button after removing title bar

--- DupDetector/GridWindow.xaml.cs
+++ DupDetector/GridWindow.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace DupDetector
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for GridWindow.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class GridWindow : Window
     {
-        public Window1()
+        public GridWindow()
         {
             InitializeComponent();
         }
--- DupDetector/MainWindow.xaml.cs
+++ DupDetector/MainWindow.xaml.cs
@@ -20,9 +20,27 @@ namespace DupDetector
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainVM ViewModel { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
         }
     }
 }
